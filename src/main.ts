ae3fba668670ac7825184cd494996aac75388284
ehtisham <ask.ituser@gmail.com>
2026-02-03 07:30:36 +0500

    Refactor code structure for improved readability and maintainability

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://localhost:4000'],
+    origin: ['http://localhost:3000', 'http://localhost:5500'],
     credentials: true
   });
   
